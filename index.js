eceff065dfb9bae8eaf288b00f9c4c8b0a24f4ec
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-10-15 13:53:52 +0200

    New http server.

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var http = require('http');
 
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+server.listen(8080);
